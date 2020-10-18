cff72b8ec1feccf7314a02fa07f4ef998557a878
timofeychik-roman <timofeychik.roman@yandex.ru>
2020-10-18 03:08:03 +0300

    init: create function reverse()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0)
+        n *= -1;
+  let num = String(n);
+  num = num.split("").reverse().join("");
+
+  num = Number(num);
+  return num;
 }
